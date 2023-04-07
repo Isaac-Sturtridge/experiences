d1c4b6e8074d93f9c2b9e5fc0ae7c9e213798688
Isaac Sturtridge <eyes330@yahoo.co.uk>
2023-04-07 15:48:02 +0800

    hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Hero() {
     return (
-        <div>
-            <img src={require("../images/photogrid.png")}/>
-        </div>
+        <section className='hero'>
+            <img className='hero--photo' src={require("../images/photogrid.png")}/>
+            <h1 className='hero--header'>Online Experiences</h1>
+            <p className='hero--text'>Join unique activities led by highly skilled corporate clients who will provide value to you through their expert consultancies.</p>
+        </section>
     )
 }
