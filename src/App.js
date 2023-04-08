ee076c349bd9df71f0981fdf65f8f8fcfade37f4
Isaac Sturtridge <eyes330@yahoo.co.uk>
2023-04-08 20:34:03 +0800

    sold out and props

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import logo from './logo.svg';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
 import Card from './components/card';
+import data from './data';
 import './App.css';
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card 
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar/>
       <Hero />
-      <Card 
-        img="katie-zaferes.jpg"
-        rating="5.0"
-        reviewCount={6}
-        country="Kyrgyzstan"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      <section className='cards-list'>
+      {cards}
+      </section>
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -3,12 +3,13 @@ import React from "react";
 export default function Card(props) {
     return (
         <div className="card">
+            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
             <img src={require(`../images/${props.img}`)} className="card--image"/>
             <div className="card--stats">
                 <img src={require("../images/star.png")} className="card--star"/>
                 <span>{props.rating}</span>
                 <span className="grey">({props.reviewCount}) .</span>
-                <span className="grey">{props.country}</span>
+                <span className="grey">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> per person</p>
