e15826c42178395dcf574a79615d16e851a31181
Isaac Sturtridge <eyes330@yahoo.co.uk>
2023-04-08 10:50:31 +0800

    added card and styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
+import Card from './components/card';
 import './App.css';
 
 function App() {
@@ -9,6 +10,14 @@ function App() {
     <div>
       <Navbar/>
       <Hero />
+      <Card 
+        img="katie-zaferes.jpg"
+        rating="5.0"
+        reviewCount={6}
+        country="Kyrgyzstan"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
